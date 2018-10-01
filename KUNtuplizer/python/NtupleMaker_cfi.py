a516db9d16c7301ca54e150726de4f78952d66ea
skhalil <skhalil@fnal.gov>
2018-10-01 13:30:32 -0500

    cleanup

--- KUNtuplizer/python/NtupleMaker_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-makeNtuples = cms.EDAnalyzer('KUNtuplizer',
-    genParts    = cms.InputTag("packedGenParticles"),    
-    vertices    = cms.InputTag("offlineSlimmedPrimaryVertices"),
-    puInfo      = cms.InputTag("slimmedAddPileupInfo"),
-)
